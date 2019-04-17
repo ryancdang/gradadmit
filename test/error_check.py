098b68cb6059c66974e92aa3ec352918a8648d80
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2019-04-17 14:28:48 -0400

    Add files via upload

--- test/error_check.py
+++ test/error_check.py
@@ -1,28 +1,64 @@
 def error_check(m, main_input):
-    if m == '1':
-        # gre score must be between 0 and 340
-        if int(main_input) >= 0 and int(main_input) <= 340:
-            return True
+    #err_msg = ''
+    try:
+        if m == '1':
+            # gre score must be between 0 and 340
+            if int(main_input) >= 0 and int(main_input) <= 340:
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
+        elif m == '2':
+            # toefl score must be between 0 and 120
+            if int(main_input) >= 0 and int(main_input) <= 120:
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
+        elif m == '3':
+            # uni rating must be between 0 and 5
+            if int(main_input) >= 0 and int(main_input) <= 5:
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
+        elif m == '4':
+            # sop must be between 0.00 and 5.00
+            if float(main_input) >= 0.00 and float(main_input) <= 5.00:
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
+        elif m == '5':
+            # lor must be between 0.00 and 5.00
+            if float(main_input) >= 0.00 and float(main_input) <= 5.00:
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
+        elif m == '6':
+            # cgpa must be between 0.00 and 10.00
+            if float(main_input) >= 0.00 and float(main_input) <= 10.00:
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
+        elif m == '7':
+            # res exp must be yes, no, y, n
+            if main_input.lower() == 'yes' or main_input.lower() == 'y' or main_input.lower() == 'no' or main_input.lower() == 'n':
+                return True
+            elif int(main_input) == -1:
+                return True
+            else:
+                return False
         else:
             return False
-    elif m == '2':
-        # toefl score must be between 0 and 120
-        if int(main_input) >= 0 and int(main_input) <= 120:
-            return True
-        else:
-            return False
-    elif m == '3':
-        # cgpa must be between 0 and 10
-        if float(main_input) >= 0 and float(main_input) <= 10.0000:
-            return True
-        else:
-            return False
-    elif m == '4':
-        # research experience must be yes/no/y/n
-        main_input = main_input.lower()
-        if main_input == 'yes' or main_input == 'y' or main_input == 'no' or main_input == 'n':
-            return True
-        else:
-            return False
-    else:
+    except:
         return False
+        # print message
